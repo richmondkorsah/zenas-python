4c080613228c05749c0e1ae883ce15a9bd98f28c
Richmond Korsah <155199945+richmondkorsah@users.noreply.github.com>
2024-11-19 09:06:51 -0800

    Add assignment files

--- vote.py
+++ vote.py
@@ -1,12 +1,11 @@
 # Write a program that asks for the user’s age and country. If they’re at least 18 and the country is "Ghana," print "You can vote." Otherwise, print "You cannot vote."
 
-# PLEASE VOTE FOR THE NEW FORCE. CHEDDAR ALL THE WAY
 
 def vote(age):
     if age > 18:
-        return "You are eligible to vote" # Vote for The New Force 
+        return "You are eligible to vote"
     else:
-        return "You are not eligible to vote" # Vote for The New Force in 2026
+        return "You are not eligible to vote"
     
 
 age = int(input("Enter your age: "))
